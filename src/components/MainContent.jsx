1dff2594676c13f20d98e8bfdd0f0060dd7bdb04
erfan <mahdi.farahmandsafa@gmail.com>
2025-04-21 00:16:34 +0330

    add Banner, portfolio and services to main content

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
+import { SimpleGrid } from "@chakra-ui/react";
+import Banner from "./Banner";
+import Portfolio from "./Portfolio";
+import Services from "./Services";
 
 const MainContent = () => {
   return (
-    <div>MainContent</div>
-  )
-}
+    <SimpleGrid columns={1}>
+      <Banner />
+      <Portfolio />
+      <Services />
+    </SimpleGrid>
+  );
+};
 
-export default MainContent
+export default MainContent;
