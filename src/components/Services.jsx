797a3436d4636464c3209b050f7fb641e0f44634
erfan <mahdi.farahmandsafa@gmail.com>
2025-04-21 00:16:53 +0330

    create services component

---
+++ src/components/Services.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import SectionName from './SectionName'
+
+const Services = () => {
+  return (
+    <>
+    <SectionName title="Services"/>
+    </>
+  )
+}
+
+export default Services
