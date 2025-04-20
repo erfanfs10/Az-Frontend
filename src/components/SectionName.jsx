ab445530998d15ca8ffd92bc5ae701a83272d1c8
erfan <mahdi.farahmandsafa@gmail.com>
2025-04-21 00:15:04 +0330

    create section name component to use it again and separate different sections of web

---
+++ src/components/SectionName.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { Text, Flex, Separator } from "@chakra-ui/react";
+
+const SectionName = ({ title }) => {
+  return (
+    <>
+      <Separator size="md" my="30px" />
+      <Flex justifyContent="center">
+        <Text
+          borderRadius="lg"
+          shadow="md"
+          width="fit-content"
+          bgColor="gray.300"
+          px="6"
+          py="2"
+          fontSize="xl"
+          fontWeight="bold"
+        >
+          {title}
+        </Text>
+      </Flex>
+    </>
+  );
+};
+
+export default SectionName;
