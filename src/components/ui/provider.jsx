66fff0659c043e11092d67040056e703a0558ecb
erfan <mahdi.farahmandsafa@gmail.com>
2025-04-20 22:48:42 +0330

    add chakra-ui to project

---
+++ src/components/ui/provider.jsx
@@ -0,0 +1,13 @@
+"use client";
+
+import { ChakraProvider } from "@chakra-ui/react";
+import { ColorModeProvider } from "./color-mode";
+import { system } from "./theme.js";
+
+export function Provider(props) {
+  return (
+    <ChakraProvider value={system}>
+      <ColorModeProvider forcedTheme="dark" {...props} />
+    </ChakraProvider>
+  );
+}
