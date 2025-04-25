cf9ecc1e8431d6a74574954043d36e8899928525
erfan <mahdi.farahmandsafa@gmail.com>
2025-04-25 21:47:33 +0330

    add panel color

--- src/components/ui/theme.js
+++ src/components/ui/theme.js
@@ -6,26 +6,32 @@ const config = defineConfig({
       colors: {
         bg: {
           DEFAULT: {
-            value: { _light: "{colors.gray.50}", _dark: "{colors.gray.900}" }, // Custom dark background
+            value: { _light: "{colors.gray.200}", _dark: "{colors.gray.900}" }, // Custom dark background
           },
           subtle: {
-            value: { _light: "{colors.gray.50}", _dark: "{colors.gray.900}" }, // Custom dark subtle background
+            value: { _light: "{colors.gray.50}", _dark: "{colors.gray.800}" }, // Custom dark subtle background
           },
           muted: {
             value: { _light: "{colors.gray.100}", _dark: "{colors.gray.600}" }, // Custom dark muted background
           },
+          panel: {
+            value: { _light: "{colors.gray.100}", _dark: "{colors.gray.800}" },
+          },
         },
         fg: {
           DEFAULT: {
-            value: { _light: "{colors.gray.900}", _dark: "{colors.gray.50}" }, // Custom dark text color
+            value: { _light: "{colors.gray.900}", _dark: "{colors.gray.50}" },
+          },
+          subtle: {
+            value: { _light: "{colors.gray.600}", _dark: "{colors.gray.100}" },
           },
           muted: {
-            value: { _light: "{colors.gray.600}", _dark: "{colors.gray.100}" }, // Custom dark muted text
+            value: { _light: "{colors.gray.600}", _dark: "{colors.gray.100}" },
           },
         },
         border: {
           DEFAULT: {
-            value: { _light: "{colors.gray.200}", _dark: "#404040" }, // Custom dark border
+            value: { _light: "{colors.gray.200}", _dark: "#404040" },
           },
         },
       },
