3ecf831e264b41114b4e5e28d61015743c5b3dec
erfan <mahdi.farahmandsafa@gmail.com>
2025-04-25 09:25:40 +0330

    replace each service with card component and using flex

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,12 +1,66 @@
-import React from 'react'
-import SectionName from './SectionName'
+import React from "react";
+import { Avatar, Card, Flex } from "@chakra-ui/react";
+import SectionName from "./SectionName";
 
 const Services = () => {
   return (
     <>
-    <SectionName title="Services"/>
+      <Flex direction="column">
+        <SectionName title="Services" />
+        <Flex
+          mt="20px"
+          direction="row"
+          wrap="wrap"
+          gap="4"
+          justifyContent="space-between"
+          alignItems="center"
+        >
+          <Card.Root width="400px">
+            <Card.Body gap="2">
+              <Avatar.Root size="lg" shape="rounded">
+                <Avatar.Image src="https://picsum.photos/200/300" />
+                <Avatar.Fallback name="Nue Camp" />
+              </Avatar.Root>
+              <Card.Title mt="2">Nue Camp</Card.Title>
+              <Card.Description>
+                This is the card body. Lorem ipsum dolor sit amet, consectetur
+                adipiscing elit. Curabitur nec odio vel dui euismod fermentum.
+                Curabitur nec odio vel dui euismod fermentum.
+              </Card.Description>
+            </Card.Body>
+          </Card.Root>
+          <Card.Root width="400px">
+            <Card.Body gap="2">
+              <Avatar.Root size="lg" shape="rounded">
+                <Avatar.Image src="https://picsum.photos/200/300" />
+                <Avatar.Fallback name="Nue Camp" />
+              </Avatar.Root>
+              <Card.Title mt="2">Nue Camp</Card.Title>
+              <Card.Description>
+                This is the card body. Lorem ipsum dolor sit amet, consectetur
+                adipiscing elit. Curabitur nec odio vel dui euismod fermentum.
+                Curabitur nec odio vel dui euismod fermentum.
+              </Card.Description>
+            </Card.Body>
+          </Card.Root>
+          <Card.Root width="400px">
+            <Card.Body gap="2">
+              <Avatar.Root size="lg" shape="rounded">
+                <Avatar.Image src="https://picsum.photos/200/300" />
+                <Avatar.Fallback name="Nue Camp" />
+              </Avatar.Root>
+              <Card.Title mt="2">Nue Camp</Card.Title>
+              <Card.Description>
+                This is the card body. Lorem ipsum dolor sit amet, consectetur
+                adipiscing elit. Curabitur nec odio vel dui euismod fermentum.
+                Curabitur nec odio vel dui euismod fermentum.
+              </Card.Description>
+            </Card.Body>
+          </Card.Root>
+        </Flex>
+      </Flex>
     </>
-  )
-}
+  );
+};
 
-export default Services
+export default Services;
