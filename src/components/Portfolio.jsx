6028761dd96c5fa951675f111aa2f6836d5df36e
erfan <mahdi.farahmandsafa@gmail.com>
2025-04-25 21:44:54 +0330

    add borderRadius to tabs

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Grid, GridItem, VStack, Image, Tabs } from "@chakra-ui/react";
+import { Grid, GridItem, VStack, Image, Tabs, Flex } from "@chakra-ui/react";
 import SectionName from "./SectionName";
 import Eth from "../assets/eth.webp";
 import Trx from "../assets/trx.webp";
@@ -15,21 +15,26 @@ const Portfolio = () => {
           <SectionName title="Portfolio" />
         </GridItem>
         <GridItem colSpan={{ base: 6, sm: 6, md: 3, lg: 3, xl: 3 }}>
-          <VStack>
+          <Flex justifyContent="center" alignItems="center">
             <Tabs.Root
               value={pic}
               onValueChange={(e) => setPic(e.value)}
-              orientation="horizontal"
               variant="plain"
             >
-              <Tabs.List bg="bg.muted" rounded="l3" p="1">
-                <Tabs.Trigger value={Eth}>ETH</Tabs.Trigger>
-                <Tabs.Trigger value={Trx}>TRX</Tabs.Trigger>
-                <Tabs.Trigger value={Sol}>SOL</Tabs.Trigger>
-                <Tabs.Indicator rounded="l2" />
+              <Tabs.List bg="bg.muted" p="2" borderRadius="lg" shadow="md">
+                <Tabs.Trigger borderRadius="lg" value={Eth}>
+                  ETH
+                </Tabs.Trigger>
+                <Tabs.Trigger borderRadius="lg" value={Trx}>
+                  TRX
+                </Tabs.Trigger>
+                <Tabs.Trigger borderRadius="lg" value={Sol}>
+                  SOL
+                </Tabs.Trigger>
+                <Tabs.Indicator borderRadius="lg" />
               </Tabs.List>
             </Tabs.Root>
-          </VStack>
+          </Flex>
         </GridItem>
         <GridItem colSpan={{ base: 6, sm: 6, md: 3, lg: 3, xl: 3 }}>
           <VStack>
