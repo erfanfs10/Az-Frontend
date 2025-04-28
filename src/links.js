86def24e75417c041bda2e80b975bd5b166e4b30
erfan <mahdi.farahmandsafa@gmail.com>
2025-04-28 14:33:38 +0330

    dynamic links

---
+++ src/links.js
@@ -0,0 +1,8 @@
+export const links = [
+  { title: "Portfolio", section: "portfolio" },
+  { title: "Services", section: "services" },
+  { title: "Work With Us", section: "workWithUs" },
+  { title: "Comments", section: "comments" },
+  { title: "Our Team", section: "ourTeam" },
+  { title: "Contact", section: "contact" },
+];
