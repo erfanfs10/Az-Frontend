660d474e62bf113f1a0a67fc39382dbd0e8d722c
erfan <mahdi.farahmandsafa@gmail.com>
2025-04-28 19:38:03 +0330

    get services from backend

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,118 +1,60 @@
-import React from "react";
+import { useState, useEffect } from "react";
 import { Avatar, Card, Grid, GridItem } from "@chakra-ui/react";
+import apiClient, { StaticURL } from "../api/client";
+import { CanceledError } from "axios";
 import SectionName from "./SectionName";
 
 const Services = () => {
+  const [services, setServices] = useState(null);
+
+  useEffect(() => {
+    const controller = new AbortController();
+    apiClient
+      .get("services/", {
+        signal: controller.signal,
+      })
+      .then((res) => {
+        setServices(res.data.services);
+      })
+      .catch((err) => {
+        if (err instanceof CanceledError) return;
+      });
+    return () => controller.abort();
+  }, []);
+
   return (
     <>
       <Grid templateColumns="repeat(4, 1fr)" gap="3" id="services">
         <GridItem colSpan={4} mb="30px">
           <SectionName title="Services" />
         </GridItem>
-        <GridItem colSpan={{ base: 4, sm: 4, md: 3, lg: 1, xl: 1 }}>
-          <Card.Root
-            size={{
-              base: "sm",
-              sm: "sm",
-              md: "md",
-              lg: "lg",
-              xl: "lg",
-            }}
-            borderRadius="lg"
-            shadow="md"
+        {services?.map((service) => (
+          <GridItem
+            key={service.id}
+            colSpan={{ base: 4, sm: 4, md: 3, lg: 1, xl: 1 }}
           >
-            <Card.Body gap="2">
-              <Avatar.Root size="lg" shape="rounded">
-                <Avatar.Image src="https://picsum.photos/200/300" />
-                <Avatar.Fallback name="Nue Camp" />
-              </Avatar.Root>
-              <Card.Title mt="2">Nue Camp</Card.Title>
-              <Card.Description>
-                This is the card body. Lorem ipsum dolor sit amet, consectetur
-                adipiscing elit. Curabitur nec odio vel dui euismod fermentum.
-                Curabitur nec odio vel dui euismod fermentum.
-              </Card.Description>
-            </Card.Body>
-          </Card.Root>
-        </GridItem>
-        <GridItem colSpan={{ base: 4, sm: 4, md: 3, lg: 1, xl: 1 }}>
-          <Card.Root
-            size={{
-              base: "sm",
-              sm: "sm",
-              md: "md",
-              lg: "lg",
-              xl: "lg",
-            }}
-            borderRadius="lg"
-            shadow="md"
-          >
-            <Card.Body gap="2">
-              <Avatar.Root size="lg" shape="rounded">
-                <Avatar.Image src="https://picsum.photos/200/300" />
-                <Avatar.Fallback name="Nue Camp" />
-              </Avatar.Root>
-              <Card.Title mt="2">Nue Camp</Card.Title>
-              <Card.Description>
-                This is the card body. Lorem ipsum dolor sit amet, consectetur
-                adipiscing elit. Curabitur nec odio vel dui euismod fermentum.
-                Curabitur nec odio vel dui euismod fermentum.
-              </Card.Description>
-            </Card.Body>
-          </Card.Root>
-        </GridItem>
-        <GridItem colSpan={{ base: 4, sm: 4, md: 3, lg: 1, xl: 1 }}>
-          <Card.Root
-            size={{
-              base: "sm",
-              sm: "sm",
-              md: "md",
-              lg: "lg",
-              xl: "lg",
-            }}
-            borderRadius="lg"
-            shadow="md"
-          >
-            <Card.Body gap="2">
-              <Avatar.Root size="lg" shape="rounded">
-                <Avatar.Image src="https://picsum.photos/200/300" />
-                <Avatar.Fallback name="Nue Camp" />
-              </Avatar.Root>
-              <Card.Title mt="2">Nue Camp</Card.Title>
-              <Card.Description>
-                This is the card body. Lorem ipsum dolor sit amet, consectetur
-                adipiscing elit. Curabitur nec odio vel dui euismod fermentum.
-                Curabitur nec odio vel dui euismod fermentum.
-              </Card.Description>
-            </Card.Body>
-          </Card.Root>
-        </GridItem>
-        <GridItem colSpan={{ base: 4, sm: 4, md: 3, lg: 1, xl: 1 }}>
-          <Card.Root
-            size={{
-              base: "sm",
-              sm: "sm",
-              md: "md",
-              lg: "lg",
-              xl: "lg",
-            }}
-            borderRadius="lg"
-            shadow="md"
-          >
-            <Card.Body gap="2">
-              <Avatar.Root size="lg" shape="rounded">
-                <Avatar.Image src="https://picsum.photos/200/300" />
-                <Avatar.Fallback name="Nue Camp" />
-              </Avatar.Root>
-              <Card.Title mt="2">Nue Camp</Card.Title>
-              <Card.Description>
-                This is the card body. Lorem ipsum dolor sit amet, consectetur
-                adipiscing elit. Curabitur nec odio vel dui euismod fermentum.
-                Curabitur nec odio vel dui euismod fermentum.
-              </Card.Description>
-            </Card.Body>
-          </Card.Root>
-        </GridItem>
+            <Card.Root
+              size={{
+                base: "sm",
+                sm: "sm",
+                md: "md",
+                lg: "lg",
+                xl: "lg",
+              }}
+              borderRadius="lg"
+              shadow="md"
+            >
+              <Card.Body gap="2">
+                <Avatar.Root size="lg" shape="rounded">
+                  <Avatar.Image src={StaticURL+service.image} />
+                  <Avatar.Fallback name="Nue Camp" />
+                </Avatar.Root>
+                <Card.Title mt="2">{service.title}</Card.Title>
+                <Card.Description>{service.description}</Card.Description>
+              </Card.Body>
+            </Card.Root>
+          </GridItem>
+        ))}
       </Grid>
     </>
   );
