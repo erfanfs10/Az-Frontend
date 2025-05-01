32f9fc188bc734633c47b4b91bdf1a76ee945f0e
erfan <mahdi.farahmandsafa@gmail.com>
2025-05-01 13:06:31 +0330

    change default theme to blue

--- src/components/ui/theme.js
+++ src/components/ui/theme.js
@@ -1,6 +1,11 @@
 import { createSystem, defaultConfig, defineConfig } from "@chakra-ui/react";
 
 const config = defineConfig({
+  globalCss: {
+    html: {
+      colorPalette: "blue",
+    },
+  },
   theme: {
     semanticTokens: {
       colors: {
