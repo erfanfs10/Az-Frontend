5b0616d6da94adda08a461f34332e4c6ee8a32fa
erfan <mahdi.farahmandsafa@gmail.com>
2025-05-01 13:07:44 +0330

    add scroll to comments

--- src/components/Comments.jsx
+++ src/components/Comments.jsx
@@ -6,6 +6,7 @@ import {
   Card,
   Grid,
   GridItem,
+  Flex,
 } from "@chakra-ui/react";
 import apiClient, { StaticURL } from "../api/client";
 import { CanceledError } from "axios";
@@ -31,46 +32,44 @@ const Comments = () => {
 
   return (
     <>
-      <Grid templateColumns="repeat(4, 1fr)" gap="3" id="comments">
-        <GridItem colSpan={4} mb="30px">
-          <SectionName title="Our Customers Opinions" />
+      <Grid templateColumns="repeat(4, 1fr)" gap="3">
+        <GridItem colSpan={4}>
+          <SectionName title="Our Customers Opinions" id="comments" />
         </GridItem>
-
-        {comments?.map((comment) => (
-          <GridItem
-            key={comment.id}
-            colSpan={{ base: 4, sm: 4, md: 3, lg: 1, xl: 1 }}
-          >
-            <Card.Root
-              size={{
-                base: "sm",
-                sm: "sm",
-                md: "md",
-                lg: "lg",
-                xl: "lg",
-              }}
-              borderRadius="lg"
-              shadow="md"
-            >
-              <Card.Body gap="2">
-                <Card.Title mt="2">{comment.title}</Card.Title>
-                <Card.Description>{comment.description}</Card.Description>
-              </Card.Body>
-              <Card.Footer justifyContent="flex-start">
-                <HStack gap="4">
-                  <Avatar.Root shape="rounded" size="lg">
-                    <Avatar.Fallback />
-                    <Avatar.Image src={StaticURL+comment.avatar}/>
-                  </Avatar.Root>
-                  <Text fontWeight="bold" fontSize="lg">
-                    {comment.name}
-                  </Text>
-                </HStack>
-              </Card.Footer>
-            </Card.Root>
-          </GridItem>
-        ))}
       </Grid>
+      <Flex scrollSnapType="x" overflowX="auto" gap={4} p={4}>
+        {comments?.map((comment) => (
+          <Card.Root
+            key={comment.id}
+            size={{
+              base: "sm",
+              sm: "sm",
+              md: "md",
+              lg: "lg",
+              xl: "lg",
+            }}
+            minW="300px"
+            borderRadius="lg"
+            shadow="md"
+          >
+            <Card.Body gap="2">
+              <Card.Title mt="2">{comment.title}</Card.Title>
+              <Card.Description>{comment.description}</Card.Description>
+            </Card.Body>
+            <Card.Footer justifyContent="flex-start">
+              <HStack gap="4">
+                <Avatar.Root shape="rounded" size="lg">
+                  <Avatar.Fallback />
+                  <Avatar.Image src={StaticURL + comment.avatar} />
+                </Avatar.Root>
+                <Text fontWeight="bold" fontSize="lg">
+                  {comment.name}
+                </Text>
+              </HStack>
+            </Card.Footer>
+          </Card.Root>
+        ))}
+      </Flex>
     </>
   );
 };
